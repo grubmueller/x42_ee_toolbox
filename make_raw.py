72a930b5cf50b321b870c5beffa2989af30519d6
Grubmüller, Michael <michael.grubmueller@gmail.com>
2025-05-12 22:00:53 +0200

    Add initial conversion suite

--- make_raw.py
+++ make_raw.py
@@ -1,25 +1,12 @@
 # Michael Grubmüller
-# September, 2024
+# May, 2025
+
+# ATTENTION: currently only working on macOS
 
 from glob import glob
 import os
-import shutil
 
-# Clear /raw folder
-raw_list = glob('raw/*.raw')
-print(f'Delete {len(raw_list)} old raw files.')
-for raw_file in raw_list:
-    os.remove(raw_file)
-
-# Make raw files of all .hp42s source files
+rawconv_dir = 'rawconv-mac'
 source_list = glob('**/*.hp42s', recursive=True)
-print(f'Translate {len(source_list)} source files.')
 for source_file in source_list:
-    os.system(f'txt2raw {source_file}')
-    
-# Move all raw files to /raw directory
-os.makedirs('raw', exist_ok=True)
-raw_list = glob('**/*.raw', recursive=True)
-print(f'Move {len(raw_list)} raw files.')
-for raw_file in raw_list:
-    shutil.move(raw_file, 'raw')
+    os.system(f'{rawconv_dir}/txt2raw {source_file}')
